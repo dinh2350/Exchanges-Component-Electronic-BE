492523b8925c267c80f916f96ad7fe382e3df92c
dinh2345 <hao9x0159@gmail.com>
2020-05-20 13:30:04 +0700

    init mongodb

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
+// init express
 var app = express();
 var port = 3000;
-
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello World!"));
-
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`server listening at http://localhost:${port}`)
 );
+
+// init mongodb
+mongoose.connect("mongodb://localhost/ece", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connected mongodb");
+});
