f2550410025b42fd7623338b1ca43d1fa29585d1
dinh2345 <hao9x0159@gmail.com>
2020-05-20 15:07:52 +0700

    finish update user

--- controllers/user.js
+++ controllers/user.js
@@ -5,19 +5,35 @@ module.exports.create = async function (req, res) {
     var { name, yearOld, phone, address } = req.body;
     var newUser = new User({ name, yearOld, phone, address });
     newUser = await newUser.save();
-    if (!newUser) return new throwError("err save user");
+    if (!newUser) new throwError("err save user");
     res.status(200).send(newUser);
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (e) {
+    res.status(500).send(e);
   }
 };
 
 module.exports.getAll = async function (req, res) {
   try {
     var users = await User.find();
-    if (!users) return new throwError("err get list user");
+    if (!users) new throwError("err get list user");
     res.status(200).send(users);
   } catch (e) {
-    res.status(500).send(err);
+    res.status(500).send(e);
   }
+};
+
+module.exports.update = function (req, res) {
+  var { name, yearOld, phone, address } = req.body;
+  var { id } = req.params;
+  User.findById({ _id: id }, function (err, user) {
+    if (err) res.status(404).send("not found user");
+    user.name = name;
+    user.yearOld = yearOld;
+    user.phone = phone;
+    user.address = address;
+    user
+      .save()
+      .then((user) => res.status(200).send(user))
+      .catch((e) => res.status(500).send("err not save"));
+  });
 };
--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,8 @@
 var express = require("express");
-var { create, getAll } = require("../controllers/user");
+var { create, getAll, update } = require("../controllers/user");
 app = express();
 
 app.post("/", create);
 app.get("/", getAll);
+app.put("/:id", update);
 module.exports = app;
